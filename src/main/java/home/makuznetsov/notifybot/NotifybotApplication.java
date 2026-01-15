2e16265e1a77244de1dc5d142e6552b9d50399eb
Кузнецов Максим <MAKuznetsov@lanit.ru>
2026-01-15 17:27:00 +0300

    added files

---
+++ src/main/java/home/makuznetsov/notifybot/NotifybotApplication.java
@@ -0,0 +1,13 @@
+package home.makuznetsov.notifybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifybotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifybotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/makuznetsov/notifybot/NotifybotApplicationTests.java
@@ -0,0 +1,13 @@
+package home.makuznetsov.notifybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifybotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
